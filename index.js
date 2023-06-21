1388bfbcf0da0ce692f105d4500340ee9940f754
Vinnih-1 <vinifantal@hotmail.com>
2023-06-21 03:24:51 -0300

    :wrench: registering playback services

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import {PlaybackService} from './src/services/PlaybackService';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => PlaybackService);

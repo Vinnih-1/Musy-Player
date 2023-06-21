4c3d149d6a5889af054d1e5720ef28a00233be4a
Vinnih-1 <vinifantal@hotmail.com>
2023-06-21 03:25:08 -0300

    :white_check_mark: add event listeners

---
+++ src/services/PlaybackService.ts
@@ -0,0 +1,9 @@
+import  TrackPlayer, { Event } from 'react-native-track-player'
+
+export const PlaybackService = async function () {
+
+    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play())
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause())
+
+    console.log('Evento registrado')
+}

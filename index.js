a43e7be7809e9ad7a16d01316acde19db27df011
Vinnih-1 <vinifantal@hotmail.com>
2023-06-21 07:04:30 -0300

    :wrench: setup player and request permissions

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import App from './src/App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 import {PlaybackService} from './src/services/PlaybackService';
+import {RequestPermissions} from './src/services/PermissionsService';
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => PlaybackService);
+TrackPlayer.setupPlayer();
+RequestPermissions();

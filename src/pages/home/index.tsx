14004a81e8f588baa069a38b75028532c36e2ea6
Vinnih-1 <vinifantal@hotmail.com>
2023-06-21 07:06:20 -0300

    :wrench: implement new card music

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Background } from './styles'
+import React, { useContext, useEffect, useState } from 'react'
+import { Background, MusicList } from './styles'
 import { NavigationBar } from '../../components/navigator'
 import { FooterBar } from '../../components/footer'
+import { MusicCard } from '../../components/card'
+import { ScrollView, Text, View } from 'react-native'
+import { TrackProps, TrackerContext } from '../../contexts/track/TrackerContext'
 
 export function Home() {
+    const trackContext = useContext(TrackerContext);
+    const [tracks, setTracks] = useState<TrackProps[]>([]);
+    
+    useEffect(() => {
+        if (!trackContext) return;
+        setTracks(trackContext.tracks);
+    });
+
     return(
         <Background>
             <NavigationBar title='Musy Player'/>
+                <MusicList>
+                    {tracks.length == 0 ?(
+                    <View style={{flex: 1, height: 1.5, justifyContent: "center", alignItems: "center"}}>
+                        <Text style={{color: "white"}}>Nenhuma música encontrada na pasta</Text>
+                        <Text style={{color: "white"}}>storage/emulated/0/Music/</Text>
+                    </View>
+                    ):(
+                        tracks.map((music) => (
+                            <MusicCard key={music.url}
+                                    title={music.title}
+                                    url={music.url}
+                                    artist={music.artist}
+                            />
+                        ))
+                    )}
+                </MusicList>
             <FooterBar/>
         </Background>
     )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -3,5 +3,8 @@ import { styled } from "styled-components/native";
 export const Background = styled.SafeAreaView`
     flex: 1;
     background-color: ${props => props.theme.background};
-    justify-content: space-between;
+`
+
+export const MusicList = styled.ScrollView`
+    flex: 1;
 `

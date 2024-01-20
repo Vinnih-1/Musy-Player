928ec31b3f52d65c0f7f36e9a8f2f76ad440981e
Vinnih-1 <vinifantal@hotmail.com>
2024-01-20 09:08:17 -0300

    chore: setup player at App function

--- App.tsx
+++ App.tsx
@@ -6,9 +6,28 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MusicProvider from './src/contexts/music-player-context.tsx';
 import { useStyles } from 'react-native-unistyles';
+import TrackPlayer, {
+  AndroidAudioContentType,
+  AppKilledPlaybackBehavior,
+} from 'react-native-track-player';
 
 function App(): React.JSX.Element {
   const { theme } = useStyles();
+
+  TrackPlayer.setupPlayer({
+    androidAudioContentType: AndroidAudioContentType.Music,
+  })
+    .then(() => {
+      TrackPlayer.updateOptions({
+        android: {
+          appKilledPlaybackBehavior: AppKilledPlaybackBehavior.ContinuePlayback,
+        },
+        progressUpdateEventInterval: 1,
+      });
+    })
+    .catch(() => {
+      console.log('TrackPlayer is already initialized!');
+    });
 
   return (
     <NavigationContainer>

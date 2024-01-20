1db9de3e13d81451fe11e4242d00815f36f71825
Vinnih-1 <vinifantal@hotmail.com>
2024-01-20 09:08:52 -0300

    remove: lot of unused attributes

--- src/contexts/music-player-context.tsx
+++ src/contexts/music-player-context.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode, createContext, useEffect, useState } from 'react';
+import React, {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { MusicProps, musicScanner } from '../services/music-scanner-service';
-import TrackPlayer, {
-  AndroidAudioContentType,
-  AppKilledPlaybackBehavior,
-  Event,
-} from 'react-native-track-player';
+import TrackPlayer, { Event } from 'react-native-track-player';
 
 export const MusicContext = createContext<TrackerProps | undefined>(undefined);
 
@@ -15,130 +17,39 @@ interface MusicPropsProvider {
 interface TrackerProps {
   musics: MusicProps[];
   queue: MusicProps[];
-  selected: {
-    artist: string;
-    title: string;
-    url: string;
-    position: number;
-    duration: number;
-  };
   loading: boolean;
-  playing: boolean;
-  titleFilter: string;
 
-  setMusics: (musics: MusicProps[]) => void;
   setQueue: (queue: MusicProps[]) => void;
-  setSelected: (music: MusicProps) => void;
-
-  setFilter: (filter: string) => void;
-
-  previous: () => void;
-  next: () => void;
 }
 
 const MusicProvider = ({ children }: MusicPropsProvider) => {
-  const setMusics = (musics: MusicProps[]) => {
-    setTracker(prevState => ({
-      ...prevState,
-      musics: musics,
-    }));
-  };
-
-  const setQueue = (queue: MusicProps[]) => {
+  const setQueue = useCallback((queue: MusicProps[]) => {
     setTracker(prevState => ({
       ...prevState,
       queue: queue,
     }));
-  };
-
-  const setSelected = (music: MusicProps) => {
-    setTracker(prevState => ({
-      ...prevState,
-      selected: {
-        artist: music.artist,
-        title: music.title,
-        url: music.url,
-        duration: 0,
-        position: 0,
-      },
-    }));
-  };
-
-  const setLoading = (loading: boolean) => {
-    setTracker(prevState => ({
-      ...prevState,
-      loading: loading,
-    }));
-  };
-
-  const setFilter = (filter: string) => {
-    setTracker(prevState => ({
-      ...prevState,
-      titleFilter: filter,
-    }));
-  };
-
-  const previous = () => {
-    TrackPlayer.skipToPrevious();
-  };
-
-  const next = () => {
-    TrackPlayer.skipToNext();
-  };
+  }, []);
 
   const [tracker, setTracker] = useState<TrackerProps>({
     musics: [],
     queue: [],
-    selected: {
-      title: '',
-      artist: '',
-      url: '',
-      position: 0,
-      duration: 0,
-    },
-    loading: true,
-    playing: false,
-    titleFilter: '',
-
-    setMusics,
     setQueue,
-    setSelected,
-
-    setFilter,
-
-    previous,
-    next,
+    loading: true,
   });
 
   useEffect(() => {
     musicScanner()
       .then(scannedMusics => {
         if (scannedMusics) {
-          setMusics(scannedMusics);
-          TrackPlayer.setupPlayer({
-            androidAudioContentType: AndroidAudioContentType.Music,
-          })
-            .then(() => {
-              TrackPlayer.updateOptions({
-                android: {
-                  appKilledPlaybackBehavior:
-                    AppKilledPlaybackBehavior.ContinuePlayback,
-                },
-                progressUpdateEventInterval: 0.7,
-              });
-
-              TrackPlayer.addEventListener(Event.PlaybackState, event => {
-                setTracker(prevState => ({
-                  ...prevState,
-                  playing: event.state === 'playing' ? true : false,
-                }));
-              });
-            })
-            .catch(() => {
-              console.log('TrackPlayer is already initialized!');
-            });
+          setTracker(prevState => ({
+            ...prevState,
+            musics: scannedMusics,
+          }));
         }
-        setLoading(false);
+        setTracker(prevState => ({
+          ...prevState,
+          loading: false,
+        }));
       })
       .catch(error => console.log(error));
   }, []);
@@ -148,39 +59,54 @@ const MusicProvider = ({ children }: MusicPropsProvider) => {
       TrackPlayer.setQueue(tracker.queue).then(() => TrackPlayer.play());
     }
 
-    const progressEvent = TrackPlayer.addEventListener(
-      Event.PlaybackProgressUpdated,
+    const queueEvent = TrackPlayer.addEventListener(
+      Event.PlaybackQueueEnded,
+      () => {
+        setTracker(prevState => ({
+          ...prevState,
+          queue: [],
+          selected: undefined,
+        }));
+      },
+    );
+
+    const stateEvent = TrackPlayer.addEventListener(
+      Event.PlaybackState,
       event => {
-        const selected = tracker.queue[event.track];
-        if (selected) {
+        setTracker(prevState => ({
+          ...prevState,
+          state: event.state.toLowerCase(),
+        }));
+      },
+    );
+
+    const trackChange = TrackPlayer.addEventListener(
+      Event.PlaybackTrackChanged,
+      async event => {
+        const trackIndex = event.nextTrack;
+        const track = await TrackPlayer.getTrack(trackIndex);
+
+        if (track) {
           setTracker(prevState => ({
             ...prevState,
             selected: {
-              artist: selected.artist,
-              title: selected.title,
-              url: selected.url,
-              duration: event.duration,
-              position: event.position,
+              artist: track.artist ?? '',
+              title: track.title ?? '',
+              url: track.url,
+              playing: true,
+              duration: 0,
+              position: 0,
             },
           }));
         }
       },
     );
 
-    const queueEvent = TrackPlayer.addEventListener(
-      Event.PlaybackQueueEnded,
-      () => {
-        tracker.setQueue([]);
-        tracker.setSelected({ artist: '', title: '', url: '' });
-      },
-    );
-
     return () => {
-      progressEvent.remove();
       queueEvent.remove();
+      stateEvent.remove();
+      trackChange.remove();
     };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tracker.queue]);
 
   return (

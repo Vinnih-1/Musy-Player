9b6da702b9a8332e3ac202aa39492752747847bf
Vinnih-1 <vinifantal@hotmail.com>
2024-01-24 13:43:03 -0300

    add: feat to unfiltered musics

--- src/pages/songs/navigations/songs-navigation.tsx
+++ src/pages/songs/navigations/songs-navigation.tsx
@@ -164,48 +164,132 @@ export const SongsNavigation = () => {
   }
 
   return (
-    <ScrollView style={styles.content}>
-      {musicContext.musics.length ? (
-        musicContext.musics.map((music, index) => (
-          <View style={styles.hover} key={index}>
-            <Song.Root
-              onClick={async () => {
-                await TrackPlayer.skip(index);
-                TrackPlayer.play();
-              }}
-              onPress={() => {
-                setSelectMusic(music);
-              }}>
-              <Song.Image />
-              <Song.Details
-                name={music.title}
-                artist={music.artist}
-                isPlaying={track && track.url === music.url ? true : false}
-              />
-            </Song.Root>
-            <View style={styles.pressContent}>
-              <Music2 strokeWidth={3} color={'#FFF'} size={40} />
-              <View style={styles.musicButtons}>
-                <Play strokeWidth={3} color={'#FFF'} size={30} />
-              </View>
+    <View style={styles.content}>
+      <ScrollView>
+        {musicContext.musics.length ? (
+          musicContext.musics.map((music, index) => (
+            <View style={styles.hover} key={index}>
+              <Song.Root
+                onClick={async () => {
+                  if (selectMusic) {
+                    setSelectMusic(undefined);
+                    return;
+                  }
+
+                  await TrackPlayer.skip(index);
+                  TrackPlayer.play();
+                }}
+                onPress={() => {
+                  setSelectMusic(music);
+                }}>
+                <Song.Image />
+                <Song.Details
+                  name={music.title}
+                  artist={music.artist}
+                  isPlaying={track && track.url === music.url ? true : false}
+                />
+              </Song.Root>
+              {selectMusic?.title === music.title && (
+                <View style={styles.pressContent}>
+                  <Music2 strokeWidth={3} color={'#FFF'} size={40} />
+                  <Text style={styles.pressContentText}>{music.title}</Text>
+                  <View style={styles.musicButtons}>
+                    <TouchableOpacity
+                      onPress={() => {
+                        TrackPlayer.setQueue([music]).then(() =>
+                          TrackPlayer.play(),
+                        );
+                      }}
+                      activeOpacity={0.5}>
+                      <Play
+                        strokeWidth={3}
+                        color={theme.colors.green}
+                        size={30}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => {
+                        sheetRef.current?.snapToIndex(1);
+                      }}
+                      activeOpacity={0.5}>
+                      <ListPlus
+                        strokeWidth={3}
+                        color={theme.colors.primary}
+                        size={30}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => {
+                        Alert.alert(
+                          'Excluir música?',
+                          'Ao confirmar, você removerá esta música do seu dispositivo.',
+                          [
+                            {
+                              text: 'Confirmar',
+                              style: 'destructive',
+                            },
+                            {
+                              text: 'Cancelar',
+                              style: 'cancel',
+                            },
+                          ],
+                        );
+                      }}
+                      activeOpacity={0.5}>
+                      <Trash2
+                        strokeWidth={3}
+                        color={theme.colors.danger}
+                        size={30}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
             </View>
-          </View>
-        ))
-      ) : (
-        <SafeAreaView style={styles.warnScreen}>
-          <FileAudio color={'#FFF'} strokeWidth={2} size={150} />
-          <Text style={styles.typrography}>
-            You don't have any songs yet...
-          </Text>
-        </SafeAreaView>
-      )}
-    </ScrollView>
+          ))
+        ) : (
+          <SafeAreaView style={styles.warnScreen}>
+            <FileAudio color={'#FFF'} strokeWidth={2} size={150} />
+            <Text style={styles.typrography}>
+              You don't have any songs yet...
+            </Text>
+          </SafeAreaView>
+        )}
+      </ScrollView>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        backgroundStyle={styles.sheetStyleContainer}
+        handleIndicatorStyle={styles.sheetIndicatorStyle}
+        index={-1}>
+        <BottomSheetScrollView>
+          {Array.from(musicContext.playlists).map(([key, value]) => (
+            <Playlist.Root
+              key={key}
+              onClick={() => {
+                if (selectMusic) {
+                  musicContext.addMusicToPlaylist(value.name, selectMusic);
+                  toast(
+                    `A música ${selectMusic.title} foi adicionada a playlist ${value.name}`,
+                  );
+                  sheetRef.current?.close();
+                  setSelectMusic(undefined);
+                }
+              }}>
+              <Playlist.Content name={value.name} musics={value.musics} />
+            </Playlist.Root>
+          ))}
+        </BottomSheetScrollView>
+      </BottomSheet>
+    </View>
   );
 };
 
 const stylesheet = createStyleSheet(theme => ({
   content: {
     backgroundColor: theme.colors.background,
+    width: '100%',
+    height: '100%',
   },
   loading: {
     backgroundColor: theme.colors.background,

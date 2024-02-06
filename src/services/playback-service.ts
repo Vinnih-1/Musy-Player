2ff5baf92d5f68d3787c1a81edc9802fdb25ccbf
Vinnih-1 <vinifantal@hotmail.com>
2024-02-06 16:23:55 -0300

    add: remote skip and previous events

--- src/services/playback-service.ts
+++ src/services/playback-service.ts
@@ -4,4 +4,10 @@ import { Event } from 'react-native-track-player';
 export const PlaybackService = async () => {
   TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
   TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemoteNext, () =>
+    TrackPlayer.skipToNext(),
+  );
+  TrackPlayer.addEventListener(Event.RemotePrevious, () =>
+    TrackPlayer.skipToPrevious(),
+  );
 };
